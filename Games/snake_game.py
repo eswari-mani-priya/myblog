9cc5b952a7bc4b6ce33a99c2a239627a5f183e73
eswari-mani-priya <eswari.gangikunta@gmail.com>
2019-09-12 22:56:20 +0530

    Fixed bugs

--- Games/snake_game.py
+++ Games/snake_game.py
@@ -3,38 +3,38 @@
 import pygame
 from pygame.locals import *
 import numpy as np
-import random
 import time
-import sys
+from random import randrange
 
 
 class SnakeGame:
-    def show_apple(self, apple, apple_pos):
-        self.DISPLAYSURF.blit(apple, tuple(apple_pos))
+    def __init__(self):
+        self.width = 500
+        self.height = 500
+        self.RED = (255, 0, 0)
+        self.BLACK = (0, 0, 0)
+        self.WIN_COLOR = (153, 204, 255)
+        self.clock = pygame.time.Clock()
+        self.display = pygame.display.set_mode((self.width, self.height),0, 32)
+        self.score = 0
+        self.snake_head = [250, 250]
+        self.snake_position = [[250,250],[240,250],[230,250]]
+        self.apple = pygame.image.load('images/apple.jpg')
+        self.apple_position = [randrange(1,50)*10, randrange(1,50)*10]
 
-    def show_snake(self, snake_pos):
-        for pos in snake_pos:
-            pygame.draw.rect(self.DISPLAYSURF, self.RED, pygame.Rect(pos[0], pos[1], 10, 10))
+    def display_apple(self, display, apple_position, apple):
+        display.blit(apple, (apple_position[0], apple_position[1]))
 
-    def ate_apple(self, score):
-        apple_pos = [random.randrange(1,100)*10, random.randrange(1,100)*10]
+    def display_snake(self, snake_position):
+        for position in snake_position:
+            pygame.draw.rect(self.display, self.RED, pygame.Rect(position[0], position[1], 10, 10))
+
+    def collision_with_apple(self, apple_position, score):
+        apple_position = [randrange(1,50)*10, randrange(1,50)*10]
         score += 1
-        return apple_pos, score
+        return apple_position, score
 
-    def hit_boundaries(self, snake_head):
-        if snake_head[0] >= 1000 or snake_head[0]<0 or snake_head[1]>=500 or snake_head[1]<0:
-            return 1
-        else:
-            return 0
-
-    def  hit_with_self(self, snake_pos):
-        snake_head = snake_pos[0]
-        if snake_head in snake_pos[1:]:
-            return 1
-        else:
-            return 0
-
-    def run_snake(self, snake_head, snake_pos, apple_pos, button_dir, score):
+    def create_snake(self, snake_head, snake_position, apple_position, button_dir, score):
         if button_dir == 1:
             snake_head[0] += 10
         elif button_dir == 0:
@@ -46,57 +46,46 @@ class SnakeGame:
         else:
             pass
 
-        if snake_head == apple_pos:
-            apple_pos, score = self.ate_apple(score)
-            snake_pos.insert(0,list(snake_head))
+        if snake_head == apple_position:
+            apple_position, score = self.collision_with_apple(apple_position, score)
+            snake_position.insert(0, list(snake_head))
         else:
-            snake_pos.insert(0,list(snake_head))
-            snake_pos.pop()
-        return snake_pos, apple_pos, score
+            snake_position.insert(0,list(snake_head))
+            snake_position.pop()
+        return snake_position, apple_position, score
 
-    def is_dir_blocked(self, snake_pos, current_directions):
-        next = snake_pos[0]+current_directions
-        snake_head = snake_pos[0]
-        if self.hit_boundaries(snake_head) == 1 or self.hit_with_self(snake_pos) == 1:
+    def collision_with_boundaries(self, snake_head):
+        if snake_head[0] >= 500 or snake_head[0] < 0 or snake_head[1] >= 500 or snake_head[1] < 0:
             return 1
         else:
             return 0
 
-    def display_final_score(self, Score_display):
-        text_font = pygame.font.Font('freesansbold.ttf', 50)
-        show_text = text_font.render(Score_display, True, self.BLACK)
-        text_rect = show_text.get_rect()
-        text_rect.center = ((500, 500))
-        self.DISPLAYSURF.blit(show_text, text_rect)
-        pygame.display.update()
-        time.sleep(2)
+    def collision_with_self(self, snake_position):
+        snake_head = snake_position[0]
+        if snake_head in snake_position[1:]:
+            return 1
+        else:
+            return 0
 
-    def __init__(self):
-        pygame.init()
-        self.DISPLAYSURF = pygame.display.set_mode((1000,1000),0,32)
-        self.RED = (255, 0, 0)
-        self.BLACK = (0, 0, 0)
-        self.GREEN = (0, 255, 0)
-        self.WINDOW_COLOR = (200, 200, 200)
-        self.DISPLAYSURF.fill(self.WINDOW_COLOR)
-        apple = pygame.image.load('images/apple.jpg')
-        self.clock = pygame.time.Clock()
-        score = 0
-        #Defining apple and snake positions
-        snake_head = [500,500]
-        snake_position = [[500,500], [540, 500], [530, 500]]
-        apple_position = [random.randrange(1,100)*10, random.randrange(1,100)*10]
-        pygame.display.set_caption('Snake Game SCORE: ' + str(score))
-        collide = False
-        # button directions
+    def is_blocked_dir(self, snake_position, current_dir_vector):
+        next_step = snake_position[0] + current_dir_vector
+        snake_head = snake_position[0]
+        if self.collision_with_boundaries(snake_head) == 1 or self.collision_with_self(snake_position) == 1:
+            return 1
+        else:
+            return 0
+
+    def play_game(self, snake_head, snake_position, apple_position, apple, button_dir, score):
         prev_button_dir = 1
         button_dir = 1
-        current_directions = np.array(snake_position[0]) - np.array(snake_position[1])
+        current_dir_vector = np.array(snake_position[0]) - np.array(snake_position[1])
+        collide = False
+
         while collide is not True:
             for event in pygame.event.get():
-                if event.type == QUIT:
+                if event.type == pygame.QUIT:
                     collide = True
-                elif event.type == pygame.KEYDOWN:
+                if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT and prev_button_dir != 1:
                         button_dir = 0
                     elif event.key == pygame.K_RIGHT and prev_button_dir != 0:
@@ -107,16 +96,45 @@ class SnakeGame:
                         button_dir = 2
                     else:
                         button_dir = button_dir
-            self.show_apple(apple, apple_position)
-            self.show_snake(snake_position)
-            # New Snake, apple positions and Score
-            snake_pos, apple_pos, score = self.run_snake(snake_head, snake_position, apple_position, button_dir, score)
-            pygame.display.set_caption('Snake Game SCORE: ' + str(score))
+            self.display.fill(self.WIN_COLOR)
+            self.display_apple(self.display, apple_position, apple)
+            self.display_snake(snake_position)
+            snake_position, apple_position, score = self.create_snake(snake_head, snake_position, apple_position,
+                                                                      button_dir, score)
+            pygame.display.set_caption("Snake Game Score:"+str(score))
             pygame.display.update()
             prev_button_dir = button_dir
-            if self.is_dir_blocked(snake_pos, current_directions) == 1:
+            if self.is_blocked_dir(snake_position, current_dir_vector) == 1:
                 collide = True
+            self.clock.tick(3)
+        return score
+
+    def display_score(self, display_text):
+        text_font = pygame.font.Font('freesansbold.ttf', 35)
+        text_surf = text_font.render(display_text, True, self.BLACK)
+        text_rect = text_surf.get_rect()
+        text_rect.center = ((self.width/2), (self.height/2))
+        self.display.blit(text_surf, text_rect)
+        pygame.display.update()
+        time.sleep(2)
+
+
+    def run(self):
+        pygame.init()
+        self.display.fill(self.WIN_COLOR)
+        pygame.display.update()
+        final_score = self.play_game(self.snake_head, self.snake_position, self.apple_position, self.apple, 1,
+                                     self.score)
+        display = pygame.display.set_mode((self.width, self.height))
+        display.fill(self.WIN_COLOR)
+        pygame.display.update()
+
+        display_text = "Your Score is: "+str(final_score)
+        self.display_score(display_text)
+
         pygame.quit()
 
-SnakeGame()
 
+if __name__ == "__main__":
+    app = SnakeGame()
+    app.run()
